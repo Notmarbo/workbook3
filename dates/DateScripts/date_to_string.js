40d4f8355a3ca7a8c7a851b407f32c98b4ec16b2
Marco Sanchez <Notmarcosan@gmail.com>
2023-05-04 11:28:46 -0400

    Page 2-32 exercise 1 completed

---
+++ dates/DateScripts/date_to_string.js
@@ -0,0 +1,8 @@
+const currentDate = new Date('2023-05-04');
+currentDate.valueOf();
+currentDate.toISOString();
+currentDate.toString();
+console.log(currentDate.toString());
+console.log(currentDate.toDateString());
+console.log(currentDate.toUTCString());
+console.log(currentDate.toLocaleString());
--- dates/OffBy1Website/scripts.js/index.js
+++ dates/OffBy1Website/scripts.js/index.js
@@ -1,2 +1,20 @@
 "use strict";
+console.log("index started")
+let dateEnter = document.getElementById("dateEnter");
+let answer = document.getElementById("answer");
+const calculateBtn = document.getElementById("calculateBtn");
 
+
+window.onload = init;
+
+function init(){
+    calculateBtn.onclick =  onCalculateBtnClicked;
+    console.log("init")
+}
+
+function onCalculateBtnClicked(){
+    console.log("Btn CLicked")
+    let userDate =  new Date(dateEnter.value);
+answer.innerHTML = (userDate.toUTCString());
+    console.log(userDate);
+}
